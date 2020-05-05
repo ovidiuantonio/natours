a61e88442e49f55193356e4c05d4d04a66577486
ovidiuantonio <mateiovidiu10@gmail.com>
2020-05-05 16:33:38 +0300

    final version
    modified the select, the popup close style, updated npm scripts (added prefix, concat, compress plugins)

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const button = document.querySelector(".navigation__button");
 const body = document.querySelector("body");
 const links = document.querySelectorAll(".navigation__link");
+const popup = document.querySelector(".popup");
+const buttonsBook = document.querySelectorAll(".card__cta .btn");
 
 button.addEventListener("click", () => {
   body.classList.toggle("hide-scrollbar");
@@ -10,4 +12,19 @@ links.forEach((link) => {
   link.addEventListener("click", () => {
     button.click();
   });
+});
+
+buttonsBook.forEach((button) => {
+  button.addEventListener("click", () => {
+    body.classList.toggle("hide-scrollbar");
+  });
+});
+
+popup.addEventListener("click", (e) => {
+  const close = document.querySelector(".popup__close");
+  const location = e.target;
+  if (location.classList.contains("popup")) {
+    close.click();
+    body.classList.toggle("hide-scrollbar");
+  }
 });
